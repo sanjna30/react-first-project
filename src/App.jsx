d01e3199c9b7e15d628a043410c951f67858ec03
unknown <sanjnameena74@gmail.com>
2024-09-21 13:59:21 -0400

    hey

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 
 function App() {
+  let data = "hey hello";
+  let subject = ["English","Hindi"];
+  let profile = {
+    name: "Sanajana Meena",
+    age: 23,
+    hobbies: "Reading coding"
+  }
   return (
-    <h1>RADHEY RADHEY FROM SANAJANA MEENA</h1>
+    
+    <div>
+       <h1>{data}</h1>
+       <h1>{subject}</h1>
+       <h1>{profile.name}</h1>
+       <h1>{profile.age}</h1>
+       <h1>{profile.hobbies}</h1>
+    </div>
 
   )
 }
 
-export default App
+export default App;
